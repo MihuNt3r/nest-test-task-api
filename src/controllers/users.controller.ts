783a406e08cc3fbea4ff597bcad2e9d2199cd22f
MihuNt3r <borovykmis@gmail.com>
2025-10-26 12:06:34 +0200

    feat: Add Roles

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -15,12 +15,15 @@ import { UsersService } from 'src/services/users.service'
 import { UserDto } from '../shared/dtos/users/user'
 import { UserId } from '../domain/value-objects/ids/user-id.vo'
 import { parseRole } from '../shared/helpers/helpers'
+import { Roles } from '../decorators/roles.decorator'
+import { Role } from '../domain/value-objects/roles/role.vo'
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @Roles(Role.ADMIN)
   async createUser(@Body() dto: CreateUserDto): Promise<User> {
     const role = parseRole(dto.role);
 
@@ -28,23 +31,35 @@ export class UsersController {
   }
 
   @Get('')
+  @Roles(Role.ADMIN)
   async getUsers(): Promise<Array<UserDto>> {
     return this.usersService.getUsers();
   }
 
   @Get(':id')
+  @Roles(Role.ADMIN, Role.USER)
   async getUser(@Param('id') id: string): Promise<UserDto> {
+    // if (currentUser.role === Role.USER && currentUser.id !== id) {
+    //   throw new ForbiddenException('You can only update your own profile.');
+    // }
+
     return this.usersService.getUserById(id);
   }
 
   @Put(':id')
+  @Roles(Role.ADMIN, Role.USER)
   async updateUser(@Param('id') id: string, @Body() dto: UpdateUserDto): Promise<void> {
+    // if (currentUser.role === Role.USER && currentUser.id !== id) {
+    //   throw new ForbiddenException('You can only update your own profile.');
+    // }
+
     // const user = await this.usersService.getUserById(id);
     // await this.usersService.updateUser(user);
     throw new NotImplementedException();
   }
 
   @Delete(':id')
+  @Roles(Role.ADMIN)
   async deleteUser(@Param('id') id: string): Promise<void> {
     await this.usersService.deleteUser(UserId.create(id));
   }
